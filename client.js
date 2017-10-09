417094530d8489cae9a77017eb5fe0627b4245ac
Holly Tuhake <hollytuhake@Nicoles-MacBook-Pro-2.local>
2017-10-09 14:19:17 -0500

    complete

--- client.js
+++ client.js
@@ -4,4 +4,23 @@ $(document).ready(readyNow);
 
 function readyNow(){
   console.log('ready');
+  $("#generate").on('click',createDiv);
+  $("body").on('click', '#swap', swapColor);
+  $("body").on('click', '#del',deleteDiv);
+}
+
+i = 0;
+
+function createDiv(){
+  i = i + 1;
+  $("body").append("<div class='generated'><p>" + i + "</p><button id='swap'>" + 'Swap' + "</button><button id='del'>" + 'Delete' + "</button></div>");
+
+}
+
+function swapColor(){
+  $(this).parent().css('background-color', 'yellow');
+}
+
+function deleteDiv(){
+  $(this).parent().remove();
 }
